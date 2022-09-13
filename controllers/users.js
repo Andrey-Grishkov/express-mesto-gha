f5bd6986f09b8fa082890223f99d80911054a1d9
Андрей Гришков <andreygorm@gmail.com>
2022-09-13 20:07:38 +0300

    add patch userinfo

--- controllers/users.js
+++ controllers/users.js
@@ -39,7 +39,57 @@ const createUser = (req, res) => {
     });
 };
 
+const updateUserProfile = (req, res) => {
+  const { name, about } = req.body;
+  User.findByIdAndUpdate(
+    req.user._id,
+    { name, about },
+    { new: true, runValidators: true }
+  )
+    .then((user) => {
+      if (user === null) {
+        return res
+          .status(404)
+          .send({ message:'Ошибка 404: Пользователь не найден'});
+      }
+      return res.status(200).send({ data: user });
+    })
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        return res
+          .status(400)
+          .send({message:'Ошибка 400: Переданы некорректные данные'});
+      }
+      return res.status(500).send({message:'Ошибка 500: Что-то пошло не так'});
+    });
+}
+
+const updateUserAvatar = (req, res) => {
+  const { avatar } = req.body;
+  User.findByIdAndUpdate(
+    req.user._id,
+    { avatar },
+    { new: true, runValidators: true }
+  )
+    .then((user) => {
+      if (user === null) {
+        return res
+          .status(404)
+          .send({ message:'Ошибка 404: Пользователь не найден'});
+      }
+      return res.status(200).send({ data: user });
+    })
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        return res
+          .status(400)
+          .send({message:'Ошибка 400: Переданы некорректные данные'});
+      }
+      return res.status(500).send({message:'Ошибка 500: Что-то пошло не так'});
+    });
+}
+
 module.exports = {
-  getUsers, getUserById, createUser
+  getUsers, getUserById, createUser, updateUserProfile, updateUserAvatar
 }
 
--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,13 @@
 const routerUsers = require('express').Router();
 
 const {
-  getUsers, getUserById, createUser
+  getUsers, getUserById, createUser, updateUserProfile, updateUserAvatar
 } = require('../controllers/users');
 
 routerUsers.get('/', getUsers);
 routerUsers.get('/:userId', getUserById);
 routerUsers.post('/', createUser);
+routerUsers.patch('/me', updateUserProfile);
+routerUsers.patch('/me/avatar', updateUserAvatar);
 
 module.exports = routerUsers;
