ee424a948e677b7c4c2c440dfde293f5fc9e5cd8
Андрей Гришков <andreygorm@gmail.com>
2022-10-04 22:11:19 +0300

    refactoring error 401

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,26 +1,26 @@
 const err401 = require('../utils/errorsCodes');
 const jwt = require('jsonwebtoken');
 const UnauthorizedError = require('../errors/UnauthorizedError');
+const { NODE_ENV, JWT_SECRET } = process.env;
 
 module.exports = (req, res, next) => {
-  const token = req.cookies.jwt;
+  const { authorization } = req.headers;
 
-  if (!token) {
-    throw new UnauthorizedError();
+  if (!authorization || !authorization.startsWith('Bearer ')) {
+    throw new UnauthorizedError('Неправильные почта или пароль');
   }
+
+  const token = authorization.replace('Bearer ', '');
 
   let payload;
 
   try {
-    payload = jwt.verify(token, 'super-strong-secret');
+    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
   } catch (err) {
-    res
+    return res
       .status(err401)
       .send({ message: 'Необходима авторизация' });
   }
-
   req.user = payload;
-
   next();
-  return null;
 };
