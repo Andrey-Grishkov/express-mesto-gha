b4c9e7e28b3a013af8eafe32346fac335974bd50
Андрей Гришков <andreygorm@gmail.com>
2022-10-05 10:34:49 +0300

    refactoring auth

--- app.js
+++ app.js
@@ -11,70 +11,44 @@ const auth = require('./middlewares/auth');
 const errorHandler = require('./middlewares/errorHandler');
 const cookieParser = require('cookie-parser');
 const router = require('express').Router();
-const patternUrl = /^(https?:\/\/)(www\.)?([a-z1-9-]{2,}\.)+[a-z]{2,}\/?[a-z0-9-._~:/?#[\]@!$&'()*+,;=]*/i;
+const userSign = require('./routes/userSign');
 
-mongoose.connect('mongodb://localhost:27017/mestodb', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+// const UPDATE_PARAMS = {
+//   new: true,
+//   runValidators: true,
+// };
+//
+// const secretKey = 'top-secret';
+
 
 const { PORT = 3000 } = process.env;
+
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+mongoose.connect('mongodb://localhost:27017/mestodb', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+},);
+
+app.use('/', userSign);
+
 app.use(cookieParser());
 app.use(auth);
 
-
-router.post('/signin', celebrate({
-  body: Joi.object().keys({
-    email: Joi.string().email().required(),
-    password: Joi.string().required(),
-  }),
-}), login);
-
-router.post('/signup', celebrate({
-  body: Joi.object().keys({
-    email: Joi.string().email().required(),
-    password: Joi.string().required().min(8),
-    name: Joi.string().min(2).max(30),
-    about: Joi.string().min(2).max(30),
-    avatar: Joi.string().uri().regex(patternUrl),
-  }),
-}), createUser);
-
-
-// app.post('/signin', celebrate({
-//   body: Joi.object().keys({
-//     email: Joi.string().email().required(),
-//     password: Joi.string().required(),
-//   }),
-// }), login);
-// app.post('/signup', celebrate({
-//   body: Joi.object().keys({
-//     email: Joi.string().email().required(),
-//     password: Joi.string().required(),
-//     name: Joi.string().min(2).max(30),
-//     about: Joi.string().min(2).max(30),
-//     avatar: Joi.string().uri().custom((value, helper) => {
-//       if (validator.isURL(value)) {
-//         return value;
-//       }
-//       return helper.message('Невалидный URL');
-//     }),
-//   }),
-// }), createUser);
-// app.use('/users', auth, routerUsers);
-// app.use('/cards', auth, routerCards);
-
-
-app.use(errors());
-// app.use(errorHandler);
+app.use('/users', auth, routerUsers);
+app.use('/cards', auth, routerCards);
 app.use((req, res, next) => {
   next(new NotFoundError('Ошибка 404: Страница отсутствует'));
 });
+
+
+app.use(errors());
+app.use(errorHandler);
+
 
 app.listen(PORT, () => {
   console.log(`Сервер запущен. Порт ${PORT}`);
---
+++ routes/userSign.js
@@ -0,0 +1,31 @@
+const { celebrate, Joi } = require('celebrate');
+const {login, createUser} = require('../controllers/users');
+const urlPattern = /^(https?:\/\/)(www\.)?([a-z1-9-]{2,}\.)+[a-z]{2,}\/?[a-z0-9-._~:/?#[\]@!$&'()*+,;=]*/i;
+const router = require('express').Router();
+
+router.post(
+  '/signup',
+  celebrate({
+    body: Joi.object().keys({
+      name: Joi.string().min(2).max(30),
+      about: Joi.string().min(2).max(30),
+      avatar: Joi.string().uri().regex(urlPattern),
+      email: Joi.string().required().email(),
+      password: Joi.string().required(),
+    }),
+  }),
+  createUser,
+);
+
+router.post(
+  '/signin',
+  celebrate({
+    body: Joi.object().keys({
+      email: Joi.string().email().required(),
+      password: Joi.string().required(),
+    }),
+  }),
+  login,
+);
+
+module.exports = router;
